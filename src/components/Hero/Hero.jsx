96c526ad30e57c91fd9374f46c47ed2fc86c1e5d
yashxjain <jyash5332@gmail.com>
2025-03-18 10:52:50 +0530

    new

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,32 +1,59 @@
-import React from 'react';
-import './Hero.css';
-import dark_arrow from '../../assets/white-arrow.png';
-import pdf from "../../assets/AMRIT LAW COLLEGE - ADMISSION FORM.pdf"
+import React, { useState, useEffect } from "react";
+import "./Hero.css";
+import dark_arrow from "../../assets/white-arrow.png";
+import pdf from "../../assets/AMRIT LAW COLLEGE - ADMISSION FORM.pdf";
+
+const images = [
+  require("../../assets/CollegeBuilding.png"),
+ require("../../assets/about.jpeg"),
+];
 
 const Hero = () => {
-    return (
-        <div className='hero container'>
-           <div className="notice">
-                <div className="notice-content">
-                    <p>Admissions Open for batch 2025-2026. Fill admission form  <a 
-      
-        href={pdf} // Link to the PDF file
-        target="_blank" // Opens in a new tab
-        rel="noopener noreferrer" // Improves security
-        style={{color:"blue"}}
-      >
-        HERE
-      </a></p>
-                </div>
-            </div>
-            <div className="hero-text">
-                <h1>Believes in the all-round development of its students</h1>
-                <p style={{ textAlign: "center" }}>and therefore leaves no stone unturned to ensure this.</p>
-                <button className="btn" style={{backgroundColor:"#a65320", color:"white"}}>Explore more <img src={dark_arrow} alt="" style={{ color:"white"}} /></button>
-            </div>
-            
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div
+      className="hero container"
+      style={{ backgroundImage: `url(${images[currentImage]})` }}
+    >
+      <div className="notice">
+        <div className="notice-content">
+          <p>
+            Admissions Open for batch 2025-2026. Fill admission form
+            <a
+              href={pdf}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: "blue" }}
+            >
+              HERE
+            </a>
+          </p>
         </div>
-    );
+      </div>
+      <div className="hero-text">
+        <h1>Believes in the all-round development of its students</h1>
+        <p style={{ textAlign: "center" }}>
+          and therefore leaves no stone unturned to ensure this.
+        </p>
+        <button
+          className="btn"
+          style={{ backgroundColor: "#a65320", color: "white" }}
+        >
+          Explore more{" "}
+          <img src={dark_arrow} alt="" style={{ color: "white" }} />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
